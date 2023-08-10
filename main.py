e82a547065cf465434b93bc340ef2c4eba49706f
ffserro <ffserro@hotmail.com>
2023-08-10 14:07:37 -0300

    .

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+file_ = open("logo.png", "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
+st.markdown(f"<img style='display: block; margin-left: auto; margin-right: auto; width:40%;' src='data:image/png;base64,{data_url}' alt='ComGptPatNavSSE' width='500'>", unsafe_allow_html=True)
+
+st.markdown("<h1 style='text-align: center;'>Escala de serviço generator</h1>", unsafe_allow_html=True)
