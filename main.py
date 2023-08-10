e8af5dee4f7c0641b81a4985706f75b162aab767
ffserro <ffserro@hotmail.com>
2023-08-10 14:46:08 -0300

    .

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import streamlit as st
+
+from datetime import datetime
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+host = 'smtp-mail.outlook.com'
+port = 587
+user = st.secrets['USUARIO']
+password = st.secrets['SENHA']
+server = smtplib.SMTP(host, port)
+
+server.ehlo()
+server.starttls()
+server.login(user, password)
+
 from io import BytesIO
 import base64
 
@@ -15,3 +32,17 @@ st.write('Use esta ferramenta para fazer sugestões de melhoria para o convívio
 nome_militar = st.text_input('Caso você deseje se identificar, digite o seu nome abaixo:')
 
 sugestao = st.text_area('Deixe aqui a sua sugestão: ')
+
+if st.button('Enviar'):
+
+    if nome_militar:
+        message = f'Sugestão de {nome_militar} em {datetime.now().strftime('%d/%m/%Y, às %H:%M:%S')}: \n\n{sugestao}'
+    else:
+        message = f'Sugestão anônima em {datetime.now().strftime('%d/%m/%Y, às %H:%M:%S')}: \n\n{sugestao}'
+
+    email_msg = MIMEMultipart()
+    email_msg['From'] = user
+    email_msg['To'] = 'serro@marinha.mil.br'
+    email_msg['Subject'] = 'Fale com o comando - Sugestão'
+
+    email_msg.attach(MIMEText(message, 'plain'))
