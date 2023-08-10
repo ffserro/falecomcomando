16f1b6bd4d75be84ee306aabff9d1e022fa5d4d2
ffserro <ffserro@hotmail.com>
2023-08-10 14:13:38 -0300

    .

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ data_url = base64.b64encode(contents).decode("utf-8")
 file_.close()
 st.markdown(f"<img style='display: block; margin-left: auto; margin-right: auto; width:40%;' src='data:image/png;base64,{data_url}' alt='ComGptPatNavSSE' width='500'>", unsafe_allow_html=True)
 
-st.markdown("<h1 style='text-align: center;'>Caixa de sugestões do Makão</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>CAIXA DE SUGESTÕES DO MAKÃO</h1>", unsafe_allow_html=True)
 
 st.write('Use esta ferramenta para fazer sugestões de melhoria para o convívio e o cumprimento da missão do Grupamento.')
