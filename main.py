f221d81e1e14745b09ec5b0f6d7320f61ec96907
ffserro <ffserro@hotmail.com>
2023-08-10 14:12:05 -0300

    .

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ data_url = base64.b64encode(contents).decode("utf-8")
 file_.close()
 st.markdown(f"<img style='display: block; margin-left: auto; margin-right: auto; width:40%;' src='data:image/png;base64,{data_url}' alt='ComGptPatNavSSE' width='500'>", unsafe_allow_html=True)
 
-st.markdown("<h1 style='text-align: center;'>Escala de serviço generator</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>Caixa de sugestões do Makão</h1>", unsafe_allow_html=True)
